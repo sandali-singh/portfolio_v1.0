3fd1b93f64bef1e86f3aa0d506be5116635c4099
Sandali Singh <72544086+sandali-singh@users.noreply.github.com>
2024-04-02 23:15:06 +0530

    Add files via upload

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
   <v-main class="main">
     <v-container-fluid>
+      <v-snackbar v-model="snackbar" :timeout="4000" top color="grey">
+        <span>Awesome! Your message was send</span>
+        <v-btn text color="white" @click="snackbar = false">Close</v-btn>
+      </v-snackbar>
       <v-app-bar class="px-2 mx-6 mt-5 mt-md-3 mx-md-auto">
         <v-toolbar-title>Sandali Singh</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-tabs>
-          <v-tab>About</v-tab>
-          <v-tab>Technologies</v-tab>
-          <v-tab>Project</v-tab>
-          <v-tab>Experience</v-tab>
+          <v-tab
+            ><a href="#about" @click="scrollToSection('#about')"
+              >About</a
+            ></v-tab
+          >
+          <v-tab
+            ><a href="#teck" @click="scrollToSection('#teck')"
+              >Technologies</a
+            ></v-tab
+          >
+          <v-tab><a href="#projects">Project</a></v-tab>
+          <v-tab><a href="#experience">Experience</a></v-tab>
         </v-tabs>
-        <v-btn rounded class="action_btn"> <span>Hire me! </span></v-btn>
+        <v-btn rounded class="action_btn">
+          <span><a href="#connect">Hire me! </a></span></v-btn
+        >
       </v-app-bar>
-      <v-row class="section1">
+      <v-row class="section1" id="home">
         <v-col col="12" md="12" sm="12">
           <v-container>
             <h4>
@@ -46,7 +60,7 @@
         </v-col>
       </v-row>
 
-      <v-row class="section2">
+      <v-row class="section2" id="about">
         <v-col col="12" md="12" sm="12">
           <v-container>
             <p class="mt-15 pt-15">
@@ -66,7 +80,7 @@
               impact in the realm of technology."
             </p>
 
-            <span class="mb-10">
+            <span class="mb-10 connect">
               <a href="https://www.linkedin.com/in/sandali-singh-3095b91a3/">
                 <v-icon color="black">mdi-linkedin</v-icon>
               </a>
@@ -86,7 +100,7 @@
           </v-container>
         </v-col>
       </v-row>
-      <v-row class="section3">
+      <v-row class="section3" id="teck">
         <v-col col="12" md="12" sm="12">
           <v-container class="teck-block align-center mt-10">
             <p class="green-dark">Technologies, I’m working with</p>
@@ -273,7 +287,7 @@
           </v-container>
         </v-col>
       </v-row>
-      <v-row class="section4">
+      <v-row class="section4" id="projects">
         <v-col col="12" md="12" sm="12">
           <v-container class="mt-15">
             <p class="green-dark">Projects</p>
@@ -283,9 +297,9 @@
                 v-for="(item, index) in cardData1"
                 :key="index"
                 class="mx-auto"
-                max-width="28%"
+                max-width="300"
               >
-                <v-img :src="item.imageUrl" height="200px"></v-img>
+                <v-img :src="item.imageUrl" height="180px" class="ma-2"></v-img>
 
                 <v-card-title>
                   {{ item.title }}
@@ -293,31 +307,16 @@
 
                 <v-card-subtitle>
                   {{ item.subtitle }}
-                </v-card-subtitle>
-              </v-card>
-            </div>
-            <div class="cards">
-              <v-card
-                v-for="(item, index) in cardData2"
-                :key="index"
-                class="mx-auto"
-                max-width="28%"
-              >
-                <v-img :src="item.imageUrl" height="200px"></v-img>
-
-                <v-card-title>
-                  {{ item.title }}
-                </v-card-title>
-
-                <v-card-subtitle>
-                  {{ item.subtitle }}
+                  <a :href="item.url">
+                    <v-icon small class="float-right"> mdi-arrow-right </v-icon>
+                  </a>
                 </v-card-subtitle>
               </v-card>
             </div>
           </v-container>
         </v-col>
       </v-row>
-      <v-row class="section5">
+      <v-row class="section5" id="experience">
         <v-col col="12" md="12" sm="12">
           <v-container>
             <p class="green-dark">Experiences</p>
@@ -338,11 +337,61 @@
           </v-container>
         </v-col>
       </v-row>
-      <v-row class="section6">
+      <v-row class="section6" id="connect">
         <v-col col="12" md="12" sm="12">
           <v-container>
             <p class="green-light-bold">Let’s Connect</p>
             <h2 class="green-light-big">Hire me to code your Vision</h2>
+
+            <v-form v-model="valid">
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    v-model="name"
+                    :rules="nameRules"
+                    label="Name"
+                    placeholder="Your Name"
+                    filled
+                    rounded
+                    dense
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="Email Id"
+                    placeholder="Your Email Id here"
+                    filled
+                    rounded
+                    dense
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="8">
+                  <v-textarea
+                    v-model="message"
+                    :rules="messageRules"
+                    filled
+                    name="input-7-4"
+                    label="Message"
+                    value=""
+                    placeholder="Enter here"
+                    required
+                  ></v-textarea>
+                </v-col>
+              </v-row>
+              <v-btn
+                rounded
+                :disabled="!valid"
+                class="action_btn mr-4 white--text"
+                @click="submit"
+                :loading="loading"
+              >
+                Submit
+              </v-btn>
+            </v-form>
           </v-container>
         </v-col>
       </v-row>
@@ -351,46 +400,100 @@
 </template>
 
 <script>
+import { addData } from "@/services/app.serviceAdd";
+
 export default {
   name: "HomePage",
-  data() {
-    return {
-      cardData1: [
-        {
-          imageUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
-          title: "Top western road trips",
-          subtitle: "1,000 miles of wonder",
-        },
-        {
-          imageUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
-          title: "Top western road trips",
-          subtitle: "1,000 miles of wonder",
-        },
-        {
-          imageUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
-          title: "Top western road trips",
-          subtitle: "1,000 miles of wonder",
-        },
-      ],
-      cardData2: [
-        {
-          imageUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
-          title: "Top western road trips",
-          subtitle: "1,000 miles of wonder",
-        },
-        {
-          imageUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
-          title: "Top western road trips",
-          subtitle: "1,000 miles of wonder",
-        },
-        {
-          imageUrl: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
-          title: "Top western road trips",
-          subtitle: "1,000 miles of wonder",
-        },
-      ],
-    };
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    message: "",
+    messageRules: [
+      (v) => !!v || "message is required",
+      (v) =>
+        (v && v.length <= 200) || "message must be less than 200 characters",
+    ],
+    cardData1: [
+      {
+        imageUrl: require("../assets/images/react-app.webp"),
+        title: "React-QuizApp",
+        subtitle: "React.JavaScript.Html.Css",
+        url: "https://github.com/sandali-singh/My-React-App",
+      },
+      {
+        imageUrl: require("../assets/images/usepopcorn.webp"),
+        title: "UsePopcorn",
+        subtitle: "React.JavaScript.Html.Css",
+        url: "https://github.com/sandali-singh/usePopcorn-",
+      },
+      {
+        imageUrl: require("../assets/images/forkify.webp"),
+        title: "Forkify-ReceipeApp",
+        subtitle: "JavaScript.Html.Css.Bootstrap",
+        url: "https://github.com/sandali-singh/Forkify-Recipe-application",
+      },
+      {
+        imageUrl: require("../assets/images/ninja.webp"),
+        title: "Todo-ninja",
+        subtitle: "Vue.Js.Vuetify.JavaScript.Html",
+        url: "https://ss-ninja.web.app/",
+      },
+      {
+        imageUrl: require("../assets/images/atomic.webp"),
+        title: "AtomicBlog",
+        subtitle: "React.JavaScript.Html.Css",
+        url: "https://ss-atomic-blog.web.app/",
+      },
+      {
+        imageUrl: require("../assets/images/omnifood.webp"),
+        title: "OmnifoodApp",
+        subtitle: "JavaScript.Html.Css",
+        url: "https://sandali-singh.github.io/omnifood-project/",
+      },
+    ],
+  }),
+  methods: {
+    scrollToSection(sectionId) {
+      document.querySelector(sectionId).scrollIntoView({ behavior: "smooth" });
+    },
+    submit() {
+      if (this.$refs.form.validate()) {
+        this.loading = true;
+        //console.log(this.title, this.content);
+        const message = {
+          name: this.name,
+          email: this.email,
+          message: this.message,
+        };
+        addData(message).then(() => {
+          this.loading = false;
+          this.dialog = false;
+          this.$emit("messageAdded");
+        });
+      }
+    },
   },
+  // created() {
+  //   onSnapshot(collection(db, "Databook"), (snapshot) => {
+  //     snapshot.docChanges().forEach((change) => {
+  //       if (change.type === "added") {
+  //         this.projects.push({
+  //           ...change.doc.data(),
+  //           id: change.doc.id,
+  //         });
+  //       }
+  //     });
+  //   });
+  // },
 };
 </script>
 
@@ -399,9 +502,33 @@ export default {
   box-sizing: border-box;
   /* font-family: "Montserrat", sans-serif; */
 }
-
+body {
+  scroll-behavior: smooth;
+}
 .row {
   text-align: center;
+}
+a:link,
+a:active {
+  color: #000;
+  font-size: 0.8rem;
+}
+a:visited {
+  color: #000;
+}
+a {
+  text-decoration: none;
+}
+.v-btn a:link,
+.v-btn a:active {
+  color: #fff;
+  font-size: 0.8rem;
+}
+.v-btn a:visited {
+  color: #fff;
+}
+.v-btn a {
+  text-decoration: none;
 }
 .v-app-bar {
   height: 64px;
@@ -424,6 +551,9 @@ export default {
 }
 .main {
   background-color: #f2f2f2;
+}
+p {
+  font-weight: 500;
 }
 h4 {
   color: #244633;
@@ -454,7 +584,7 @@ h2 {
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 2rem 0;
+  margin: 2rem 0 0;
 }
 .section2 {
   border-radius: 5rem;
@@ -467,6 +597,9 @@ h2 {
   align-items: center;
   left: 50%;
   transform: translate(-50%, 40%);
+}
+.connect a {
+  margin: 0.5rem;
 }
 .green-light {
   color: #3f682a;
@@ -497,8 +630,31 @@ h2 {
 }
 .cards {
   display: flex;
-  justify-content: space-evenly;
-  margin-bottom: 2rem;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 3rem;
+}
+.cards .v-card {
+  border-radius: 1rem;
+  background: #f2f2f2;
+  margin: 1rem;
+}
+.cards .v-card__title {
+  color: #244633;
+}
+.cards .v-card__subtitle {
+  color: #000;
+  text-align: left;
+  font-weight: 500;
+}
+.cards .v-card__subtitle .v-icon {
+  color: #244633;
+  font-weight: 600;
+}
+.cards .v-image__image .v-image__image--contain {
+  border-radius: 1rem;
 }
 .experience-box {
   border-radius: 1rem;
@@ -513,5 +669,14 @@ h2 {
 }
 .teck-stack {
   margin: 3rem 0;
+}
+form.v-form .row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+.theme--light.v-btn.v-btn--disabled {
+  color: #fff;
 }
 </style>
