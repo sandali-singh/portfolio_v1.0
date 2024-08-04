171f450c12e777d8e0aea35ed7c3bf42417e68b9
sandali-singh <sandalisingh1270@gmail.com>
2024-08-05 01:16:04 +0530

    Url Card Added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -93,7 +93,6 @@
               </a>
             </v-btn>
             &nbsp;&nbsp; &nbsp;
-<<<<<<< HEAD
 
             <a @click="scrollToSection('about')">
               <span class="green-light pt-4">
@@ -101,12 +100,6 @@
                 <v-icon small class="green-light">mdi-arrow-right</v-icon>
               </span>
             </a>
-=======
-            <p class="green-light pt-4">
-              Know more about me
-              <v-icon small class="green-light">mdi-arrow-right</v-icon>
-            </p>
->>>>>>> a01001b137642a0216ab9939d2d1d8b23cdec215
           </div>
           <v-img
             class="profile-img1"
@@ -540,15 +533,12 @@ export default {
     ],
     cardData1: [
       {
-<<<<<<< HEAD
         imageUrl: require("../assets/images/GoogleIOExtended.webp"),
         title: "Google IOX Extended",
         subtitle: "VueJs.JavaScript.Firebase.Nuxt",
         url: "https://github.com/oss-labs/iox24-webapp",
       },
       {
-=======
->>>>>>> a01001b137642a0216ab9939d2d1d8b23cdec215
         imageUrl: require("../assets/images/react-app.webp"),
         title: "React-QuizApp",
         subtitle: "React.JavaScript.Html.Css",
@@ -584,15 +574,12 @@ export default {
         subtitle: "JavaScript.Html.Css",
         url: "https://sandali-singh.github.io/omnifood-project/",
       },
-<<<<<<< HEAD
       {
         imageUrl: require("../assets/images/Natours.webp"),
         title: "Natours-WebApp",
         subtitle: "JavaScript.Html.Css.Sass",
         url: "https://github.com/sandali-singh/Natours-sassProject",
       },
-=======
->>>>>>> a01001b137642a0216ab9939d2d1d8b23cdec215
     ],
   }),
 
@@ -669,13 +656,10 @@ export default {
   margin: 0;
   padding: 0;
 }
-<<<<<<< HEAD
 body,
 html {
   overflow-x: hidden;
 }
-=======
->>>>>>> a01001b137642a0216ab9939d2d1d8b23cdec215
 body {
   scroll-behavior: smooth;
 }
@@ -705,11 +689,7 @@ div.container {
 .section6 div.container {
   text-align: center;
   max-width: 65%;
-<<<<<<< HEAD
   min-height: 45vh;
-=======
-  min-height: 75vh;
->>>>>>> a01001b137642a0216ab9939d2d1d8b23cdec215
 }
 .row {
   text-align: center;
@@ -836,10 +816,7 @@ h2 {
 }
 .green-light {
   color: #3f682a;
-<<<<<<< HEAD
   font-weight: 600;
-=======
->>>>>>> a01001b137642a0216ab9939d2d1d8b23cdec215
 }
 .green-light-bold {
   color: #3f682a;
